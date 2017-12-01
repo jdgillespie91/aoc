bd4e971fc9dcd5bf5732bb33bfd912b1320a857b
Jake Gillespie <jdgillespie91@gmail.com>
2017-12-01 12:33:45 +0100

    <Jake> Add command line interface to package, aoc

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aoc',
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    description='My solutions to Advent of Code 2017',
+    author='Jake Gillespie',
+    author_email='jdgillepsie91@gmail.com',
+    zip_safe=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points = {
+        'console_scripts': ['aoc=aoc.__main__:main'],
+    }
+)
